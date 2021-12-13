9e7384444c4edb1926020dedae9e4502efea4a76
st1llwater <alokpunjofficial@gmail.com>
2021-12-13 23:28:42 +0530

    Added Parallax And Variable Speed

--- sketch.js
+++ sketch.js
@@ -1,20 +1,31 @@
 let stars = [];
-let amount = 800;
+let amount = 1000;
 let speed;
+let mic;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   for (let i = 0; i < amount; i++) {
     stars[i] = new Star();
   }
+  mic = new p5.AudioIn();
+  mic.start();
+  // console.log(mic);
 }
 
 function draw() {
   background(0);
-  // let speed = map(mouseX, 0, width, 0, 80);
-  translate(width / 2, height / 2);
+  // console.log(frameCount);
+  let vol = mic.getLevel();
+  // let speed = map(vol, 0, 1, 10, 50);
+  let speed = map(mouseX, 0, width, 10, 50);
+  // console.log(speed);
+  let orientationX = map(mouseX, 0, width, 1, 3);
+  let orientationY = map(mouseY, 0, width, 1, 3);
+
+  translate(width / orientationX, height / orientationY);
   for (let i = 0; i < amount; i++) {
-    stars[i].update();
+    stars[i].update(speed);
     stars[i].show();
   }
 }
--- star.js
+++ star.js
@@ -2,11 +2,12 @@ function Star() {
   this.x = random(-width, width);
   this.y = random(-height, height);
   this.z = random(width);
+  this.pz = this.z;
 
   // let newSpeed = map(mouseX, 0, width, 0, 70);
 
-  this.update = function () {
-    this.z = this.z - 10;
+  this.update = function (speed) {
+    this.z = this.z - speed;
     if (this.z < 1) {
       this.z = width;
       this.x = random(-width, width);
@@ -15,6 +16,7 @@ function Star() {
   };
 
   this.show = function () {
+    // let color = map(mouseX, 0, width, 0, 255);
     fill(255);
     noStroke();
 
@@ -24,5 +26,13 @@ function Star() {
     let sy = map(this.y / this.z, 0, 1, 0, height);
 
     ellipse(sx, sy, r, r);
+
+    var px = map(this.x / this.pz, 0, 1, 0, width);
+    var py = map(this.y / this.pz, 0, 1, 0, height);
+
+    this.pz = this.z;
+
+    stroke(255);
+    line(px, py, sx, sy);
   };
 }
